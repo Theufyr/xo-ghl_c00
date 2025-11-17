240354dcbfbe9e4b64600b02d98b6026844ee172
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2025-11-17 11:07:51 +0100

    kata1 feat : step 5 send & display quote / update script.js

--- kata1/script.js
+++ kata1/script.js
@@ -1,5 +1,20 @@
+function addQuote(quote, author) {
+    const quoteDisplay = document.createElement("div");
+    quoteDisplay.className = "quote";
+    const pQuote = document.createElement("p");
+    pQuote.className = "text";
+    pQuote.textContent = `"${quote}"`;
+    quoteDisplay.appendChild(pQuote);
+    const pAuthor = document.createElement("p");
+    pAuthor.className = "text";
+    pAuthor.textContent = author;
+    quoteDisplay.appendChild(pAuthor);
+    document.getElementById("quote-list").appendChild(quoteDisplay);
+}
+
 document.getElementById("form_button").addEventListener('click', (event) => {
         event.preventDefault();
-        console.log(document.getElementById("input_author").value);
-        console.log(document.getElementById("input_quote").value);
+        const quoteInput = document.getElementById("input_quote").value;
+        const authorInput = document.getElementById("input_author").value;
+        addQuote(quoteInput, authorInput);
     });
